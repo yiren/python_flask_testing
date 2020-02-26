098c29e711fcb92df4cd3182a186355dae0a63a4
yiren <yiren.goo@gmail.com>
2020-02-26 22:03:03 +0800

    Basic_ReadWrite

--- app.py
+++ app.py
@@ -1,11 +1,58 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
+
+stores = [
+        {
+            'name': 'My First Store',
+            'items': [
+                {
+                    'name': 'My First Item',
+                    'price': 15.99
+                }
+            ]
+        }
+]
 
 @app.route('/')
 def home():
     return jsonify({'message': 'Hello World'})
 
+@app.route('/store', methods=['POST'])
+def create_store():
+    request_data = request.get_json()
+    new_store = {
+        'name': request_data['name'],
+        'items': []
+    }
+    stores.append(new_store)
+    return jsonify(stores)
+
+@app.route('/store')
+def get_stores():
+    return jsonify(stores)
+
+@app.route('/store/<string:name>')
+def get_store(name):
+    for store in stores:
+        if store['name'] == name:
+            return jsonify(store)
+    return jsonify({'message': 'store not found'})
+
+@app.route('/store/<string:name>/item', methods=['POST'])
+def create_item(name):
+    for store in stores:
+        if store['name'] == name:
+            request_data= request.get_json()
+            new_item={
+                'name': request_data['name'],
+                'price': request_data['price']
+            }
+            store['items'].append(new_item)
+            return jsonify(store)
+    return jsonify({'message': 'store not found'})
+
+
 
 if __name__ == '__main__': # Only called when running 'app.py,' running not from imports
-    app.run()
+    app.run(debug=True)
